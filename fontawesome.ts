a06c76b19d9dc34b1d688b364752c7530f971955
jujutsu666 <jr.traduction1@gmail.com>
2023-09-28 17:09:50 -0700

    first commit

---
+++ fontawesome.ts
@@ -0,0 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faArrowCircleLeft,
+  faArrowCircleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faArrowCircleLeft, faArrowCircleRight);
